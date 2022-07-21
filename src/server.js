6128278cceb4ec4b00901822205ee5582e842a7b
An Dakyeom <63940054+dakyeoman@users.noreply.github.com>
2022-07-21 18:21:59 +0900

    update

--- src/server.js
+++ src/server.js
@@ -1,17 +1,30 @@
 import express from "express";
+import morgan from "morgan";
+import globalRouter from "./routers/globalRouter";
+import userRouter from "./routers/userRouter";
+import videoRouter from "./routers/videoRouter";
+
 
 const PORT = 5480;
-const app = express(); 
-const handleHome = (req, res) => {
-    return res.send("<h1>I still love you.</h1>");
-};
 
-const handleLogin = (req, res) => {
-    return res.send("Login here.");
+const app = express(); 
+const logger = morgan("dev");
+app.use(logger);
+
+app.use("/", globalRouter);
+app.use("/videos", videoRouter);
+app.use("/users", userRouter);
+
+const home = (req, res) => { 
+    console.log("I will respond.");
+    return res.send("Hello");
+};
+const login = (req, res) => {
+    return res.send("login");
 }
 
-app.get("/", handleHome);
-app.get("/login", handleLogin);
+app.get("/", home);
+app.get("/login", login);
 
 const handleListening = () => 
     console.log(`✅ Server Listening on PORT http://localhost:${PORT}`);
